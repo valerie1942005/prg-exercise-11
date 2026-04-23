0190b1bac07ff36d1262caf0b153a9c1a9ed9708
valerie1942005 <hasalovavalerie@gmail.com>
2026-04-23 14:19:53 +0200

    zprava

---
+++ sorting.py
@@ -0,0 +1,10 @@
+import random
+
+def random_numbers(count, low=0, high=100):
+    return [random.randint(low, high) for _ in range(count)]
+
+if __name__ == "__main__":
+    values = random_numbers(10)  # 10 čísel v rozsahu 0–100
+    print(values)  # např. [42, 7, 91, 15, 63, 8, 57, 73, 2, 100]
+
+    # small = random_numbers(5, low=0, high=20)  # 5 čísel v rozsahu 0–20
